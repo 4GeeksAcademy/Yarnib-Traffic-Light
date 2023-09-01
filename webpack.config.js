104bdfe202336263373f6c90d98005481ec5c6c2
Yarnib <138246961+Yarnib@users.noreply.github.com>
2023-09-01 16:59:28 +0000

    Update

--- webpack.config.js
+++ webpack.config.js
@@ -33,6 +33,30 @@ module.exports = {
           use: ['babel-loader']
         },
         {
+          test: /\.(scss)$/,
+          use: [
+            {
+              loader: 'style-loader'
+            },
+            {
+              loader: 'css-loader'
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                postcssOptions: {
+                  plugins: () => [
+                    require('autoprefixer')
+                  ]
+                }
+              }
+            },
+            {
+              loader: 'sass-loader'
+            }
+          ]
+        },
+        {
           test: /\.(css)$/, use: [{
               loader: "style-loader" // creates style nodes from JS strings
           }, {
